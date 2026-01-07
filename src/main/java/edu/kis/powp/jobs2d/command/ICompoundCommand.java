0dd33818a723cce8a9bf7ccc61454b1c8bca4846
MarkdeBrunner <246325@edu.p.lodz.pl>
2026-01-07 23:27:24 +0100

    refactor: simplify ICompoundCommand interface by combining DriverCommand and Iterable to allow foreach

--- src/main/java/edu/kis/powp/jobs2d/command/ICompoundCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ICompoundCommand.java
@@ -1,12 +1,6 @@
 package edu.kis.powp.jobs2d.command;
 
-import java.util.Iterator;
-
 /**
  * Interface extending Job2dDriverCommand to execute more than one command.
  */
-public interface ICompoundCommand extends DriverCommand {
-
-    public Iterator<DriverCommand> iterator();
-
-}
+public interface ICompoundCommand extends DriverCommand, Iterable<DriverCommand> {}
